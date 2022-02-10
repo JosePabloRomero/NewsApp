84316cfb352bfbb45bf93257a19478ea4fea5938
Jose Pablo <josepablo.developer@gmail.com>
2022-02-10 13:05:03 -0500

    Searching news implementado

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "ecb894ed94a548a0aefa8b6c22279562"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
